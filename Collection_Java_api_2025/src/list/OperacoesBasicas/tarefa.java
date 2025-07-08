0b70c6b1767e14c2c7e568e27f40b8f8591528f3
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2025-07-07 22:58:45 -0300

    Aprendendo Collections (List)

---
+++ Collection_Java_api_2025/src/list/OperacoesBasicas/tarefa.java
@@ -0,0 +1,20 @@
+package list.OperacoesBasicas;
+
+public class tarefa {
+
+    private  String descricao;
+
+    public tarefa (String descricao){
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return  descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+}
