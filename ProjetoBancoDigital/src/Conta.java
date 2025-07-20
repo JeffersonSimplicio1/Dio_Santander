b48dfadf64ab72fc8c1f8db136478e2b440afc26
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2025-07-19 22:25:21 -0300

    Atualização do projeto BancoDigital

--- ProjetoBancoDigital/src/Conta.java
+++ ProjetoBancoDigital/src/Conta.java
@@ -1,27 +1,30 @@
 public abstract  class Conta implements  IConta{
-    private static final int AGENCIA_PADRAO = 0;
-    private static int SEQUENCIAL = 1;
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 10;
 
+    protected Cliente cliente;
     protected int agencia;
     protected int numero;
     protected double saldo;
 
 
 
-    public Conta(){
+    public Conta(Cliente cliente){
         this.agencia = AGENCIA_PADRAO;
         this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
     }
 
 
     public void sacar(double valor) {
-
+        this.saldo = saldo - valor;
     }
     public void depositar(double valor) {
-
+        this.saldo = saldo + valor;
     }
     public void transferir(double valor, Conta contaDestino) {
-
+        this.sacar(valor);
+        contaDestino.depositar(valor);
     }
 
     public int getAgencia() {
@@ -34,5 +37,11 @@ public abstract  class Conta implements  IConta{
 
     public double getSaldo() {
         return saldo;
+    }
+    protected void imprimirInfosComuns() {
+        System.out.printf("Cliente: %s%n", this.cliente.getNome());
+        System.out.printf("Agencia: %d%n", this.agencia);
+        System.out.printf("Numero: %d%n", this.numero);
+        System.out.printf("Saldo: %.2f%n", this.saldo);
     }
 }
--- ProjetoBancoDigital/src/Main.java
+++ ProjetoBancoDigital/src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
+        Cliente jefferson = new Cliente();
+        jefferson.setNome("Jefferson");
+
+        Conta cc = new ContaCorrente(jefferson);
+        Conta poupanca = new ContaPoupanca(jefferson);
+
+        cc.depositar(100);
+        cc.sacar(20);
+        cc.depositar(300);
+        cc.transferir(200,poupanca);
+        poupanca.sacar(50);
+
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+
+
+
 
     }
 }
